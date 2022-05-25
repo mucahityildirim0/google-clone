40b40fbe4add180dfa229c8a843493208eaaad25
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-05-25 22:01:46 +0300

    i finished navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/search"></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
